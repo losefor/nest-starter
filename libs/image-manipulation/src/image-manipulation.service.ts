d1ed325e1242157979616c64b1bd2d88317f441b
Muhammed Baqer Ahmed <moh999157@gmail.com>
2023-09-04 11:41:57 +0300

    add✨: library for image manipulation

--- libs/image-manipulation/src/image-manipulation.service.ts
+++ libs/image-manipulation/src/image-manipulation.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import sharp = require('sharp');
+
+interface Options {
+  reduceSize?: number;
+  format?: 'webp' | 'png';
+  quality?: number;
+  width?: number;
+  height?: number;
+}
 
 @Injectable()
-export class ImageManipulationService {}
+export class ImageManipulationService {
+  async toWebp(image: any): Promise<sharp.Sharp> {
+    return sharp(image, { failOn: 'none' }).toFormat('webp').webp();
+  }
+
+  async toThumbnail(image: any, options?: Options): Promise<sharp.Sharp> {
+    const metaData = await sharp(image).metadata();
+
+    return sharp(image)
+      .resize(
+        options.reduceSize
+          ? Math.floor(metaData.width * options.reduceSize)
+          : options.width,
+      )
+      .toFormat('webp')
+      .webp({
+        quality: options.quality || 100,
+      });
+  }
+}
